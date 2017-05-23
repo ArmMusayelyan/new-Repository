deca81af05d7eec769eeddf679b79c341d406297
Lenovo <a.musayelyan@inbox.ru>
2017-05-23 15:25:42 -0700

    Cragri kmaxq@

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.lenovo.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonmale;
+    private Button buttonfemale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hellow");
+        buttonmale=(Button)findViewById(R.id.buttontxa);
+        buttonfemale=(Button)findViewById(R.id.buttonaxjik);
+
+        Toast.makeText(MainActivity.this,"Խնդրում ենք նշել չեր սեռը",Toast.LENGTH_SHORT);
+
+        buttonfemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        buttonmale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
         
 
