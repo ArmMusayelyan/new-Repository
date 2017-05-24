005878d72db4bc7a1d2a68840a381e8944703ecf
Lenovo <a.musayelyan@inbox.ru>
2017-05-24 10:09:08 -0700

    Cragir@ tarmacvel e

--- app/src/main/java/com/example/lenovo/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/lenovo/myapplication/Main3Activity.java
@@ -2,6 +2,11 @@ package com.example.lenovo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItem;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.zip.Inflater;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.dzerq){
+
+        }
+return true;
+    }
+
 }
